96b21a812ffdede2d4edd490dc84febd53bfddc9
rana-elwakil7 <74627894+rna87@users.noreply.github.com>
2022-11-21 22:30:18 +1300

    Update healthcheck.js

--- backend/healthcheck.js
+++ backend/healthcheck.js
@@ -1,4 +1,20 @@
-import { get } from "http";
+const express = require("express");
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const port = process.env.PORT || 3001;
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+});
+
 
 get(`${process.env.SERVER_URL}/health`, (resp) => {
     if (resp.statusCode === 200) process.exit(0);
